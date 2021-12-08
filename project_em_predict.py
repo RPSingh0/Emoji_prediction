f9e7b5b51e0f87ca4dbc4c8219ffbb8acb053b72
RPSingh0 <rpalsingh715@gmail.com>
2021-12-08 18:07:23 +0530

    added tkinter

--- project_em_predict.py
+++ project_em_predict.py
@@ -4,52 +4,55 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import random
 import tkinter as tk
+import tkinter.font as tkFont
+from PIL import Image
+
 
 # Loading in the model
 # The model is trained on 19137 images from 3 different classes
 # Refer to the ipynb file for more info
 model = load_model("final_trained_model.h5")
 
-emoji_dictionary = {"happy": ["\U0001f600", 
+emoji_dictionary = {"happy": ["\U0001f600",
                               "\U0001f603",
-                              "\U0001f604",    
-                              "\U0001f601",  
-                              "\U0001f606",  
-                              "\U0001f923",   
-                              "\U0001f602",   
-                              "\U0001f642",   
-                              "\U0001f60A",   
-                              "\U0001f92A",    
-                              "\U0001f917",    
-                              "\U0001f60F",    
-                              "\U0001f600",   
-                              "\U0001f608",    
-                              "\U0001f921",    
-                              "\U0001f638",   
-                              "\U0001f9D1",    
-                              "\U0001f31E",],   
-                    "sad":   ["\U0001f614",    
-                              "\U0001f61F",    
-                              "\U0001f641",   
-                              "\U00002639",   
-                              "\U0001f601",   
-                              "\U0001f97A",   
-                              "\U0001f627",   
-                              "\U0001f62D",   
-                              "\U0001f629",   
-                              "\U0001f62D"],   
-                    "neutral":["\U0001f910",   
-                               "\U0001f610",   
-                               "\U0001f611",   
-                               "\U0001f636",   
-                               "\U0001f615",   
-                               "\U0001f971",   
-                               "\U0001f480",   
-                               "\U00002620",   
-                               "\U0001f482",    
-                               "\U0001f464",   
-                               "\U0001f439",   
-                               "\U0001f428"]}
+                              "\U0001f604",
+                              "\U0001f601",
+                              "\U0001f606",
+                              "\U0001f923",
+                              "\U0001f602",
+                              "\U0001f642",
+                              "\U0001f60A",
+                              "\U0001f92A",
+                              "\U0001f917",
+                              "\U0001f60F",
+                              "\U0001f600",
+                              "\U0001f608",
+                              "\U0001f921",
+                              "\U0001f638",
+                              "\U0001f9D1",
+                              "\U0001f31E", ],
+                    "sad":   ["\U0001f614",
+                              "\U0001f61F",
+                              "\U0001f641",
+                              "\U00002639",
+                              "\U0001f601",
+                              "\U0001f97A",
+                              "\U0001f627",
+                              "\U0001f62D",
+                              "\U0001f629",
+                              "\U0001f62D"],
+                    "neutral": ["\U0001f910",
+                                "\U0001f610",
+                                "\U0001f611",
+                                "\U0001f636",
+                                "\U0001f615",
+                                "\U0001f971",
+                                "\U0001f480",
+                                "\U00002620",
+                                "\U0001f482",
+                                "\U0001f464",
+                                "\U0001f439",
+                                "\U0001f428"]}
 
 
 def load_and_prepare_image(filename, image_shape=48):
@@ -78,27 +81,44 @@ def predict_class(model, filename):
             filename (str) : image path for predicting the class
     """
     img = load_and_prepare_image(filename)
-    predict_x = model.predict(tf.expand_dims(img, axis=0))
-    # print(predict_x)
-    class_name = classes[tf.argmax(predict_x[0])]
-    print(class_name, end = " ")
-    print(random.choice(emoji_dictionary[class_name]))
 
+    predict_x = model.predict(tf.expand_dims(img, axis=0))
+    class_name = classes[tf.argmax(predict_x[0])]
+    emoji_class = random.choice(emoji_dictionary[class_name])
+
+    # Opening and resizing the image to 250x250
+    print(f"filename = {filename}")
+    im1 = Image.open(filename)
+    im1 = im1.resize((250, 250))
+    im1.save(filename)  # Saving the image by same name
+
+    # Creating a tkinter window
     root = tk.Tk()
+
+    # Setting the font size and font style
+    fontStyle = tkFont.Font(family="Lucida Grande", size=40)
+
+    # Reading in the resized image
     logo = tk.PhotoImage(file=filename)
 
-    w1 = tk.Label(root, image=logo).pack(side="right")
+    # Displaying the image
+    tk.Label(root, image=logo).pack(side="top")
 
-    explanation = class_name + " " + random.choice(emoji_dictionary[class_name])
+    # Appending the predicted class and emoji into one
+    pred_ = class_name + " " + \
+        emoji_class
 
-    w2 = tk.Label(root, 
-                justify=tk.LEFT,
-                padx = 10, 
-                text=explanation).pack(side="left")
+    # Displaying the pred_
+    tk.Label(root,
+             justify=tk.LEFT,
+             padx=10,
+             text=pred_,
+             font=fontStyle).pack(side="bottom")
     root.mainloop()
 
 
 cam = cv2.VideoCapture(0)
+cv2.resizeWindow(cam, 500, 500)
 
 cv2.namedWindow("Image_capture")
 
@@ -136,4 +156,3 @@ while True:
 cam.release()
 
 cv2.destroyAllWindows()
-
